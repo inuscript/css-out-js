b6331537b8af615c1f1287e9343d7a569a5e8970
inuscript <inuscript@gmail.com>
2016-12-09 09:54:00 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import postcss from 'postcss'
+import postcssJs from 'postcss-js'
+import JSON5 from 'json5';
+
+const jsToCss = (input) => {
+  const json = JSON5.parse(input)
+  return postcss()
+    .process(json, {parser: postcssJs})
+}
+
+class Result extends Component{
+  state = { css: "" }
+  componentWillReceiveProps({input}){
+    jsToCss(input)
+      .then( (result) => {
+        this.setState({css: result.css})
+      })
+
+  }
+  render(){
+    return <pre className={"result"}>
+      <code>{this.state.css}</code>
+    </pre>
+  }
+}
 
 class App extends Component {
+  state = {
+    input: ""
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <textarea className="input"
+          placeholder={`{input: "your css js"}`}
+          value={this.state.input}
+          onChange={(e) => this.setState({input: e.target.value })}
+        />
+        <Result input={this.state.input} />
       </div>
     );
   }
